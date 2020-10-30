89781e53b21ec15708e609fdba51682bc36d7fa1
crazoter <crazoter@gmail.com>
2020-10-30 14:40:00 +0000

    Deploy website - based on 68fe7d5561115cb634b90304b77ea359639e786d

---
+++ 4aa65278.32ef3a3d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{68:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/My-Docs/blog/welcome"},{"title":"Hello","permalink":"/My-Docs/blog/hello-world"},{"title":"Hola","permalink":"/My-Docs/blog/hola"}]}')}}]);
